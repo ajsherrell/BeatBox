dec4feda9e1accca58dc4c02927c228e39c89b6c
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-13 14:41:16 -0400

    databind recyclerview, viewholder, and adapter

--- app/src/main/java/com/ajsherrell/beatbox/MainActivity.kt
+++ app/src/main/java/com/ajsherrell/beatbox/MainActivity.kt
@@ -2,9 +2,12 @@ package com.ajsherrell.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ajsherrell.beatbox.databinding.ActivityMainBinding
+import com.ajsherrell.beatbox.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +18,33 @@ class MainActivity : AppCompatActivity() {
 
         binding.recyclerView.apply {
             layoutManager = GridLayoutManager(context, 3)
+            adapter = SoundAdapter()
         }
+    }
+
+    //view holder to wire up list_item_sound
+    private inner class SoundHolder(private val binding: ListItemSoundBinding): RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    //adapter to hook up to sound holder
+    private inner class SoundAdapter(): RecyclerView.Adapter<SoundHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
+            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
+                layoutInflater,
+                R.layout.list_item_sound,
+                parent,
+                false
+            )
+            return SoundHolder(binding)
+        }
+
+        override fun getItemCount() = 0
+
+        override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
     }
 }
